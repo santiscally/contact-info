08b501f49476d775bac73e54b635a3604851422a
santiago <santiagoscally@lakaut.com.ar>
2025-05-14 12:07:07 -0300

    se cambio la url al sv

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://149.50.142.57:81/api';
 
 // Crear instancia de axios
 const api = axios.create({
